fc5d8ee04055d2db8037c0283208d1020fe51201
Aledgit <edalvarez@uc.cl>
2023-11-23 21:26:39 -0300

    Cambios

--- main.js
+++ main.js
@@ -16,7 +16,16 @@ const margins_1 = [50, 50, 50, 50]; //ZQUIERDA, DERECHA, ARRIBA, ABAJO
 const margins_2 = [50, 50, 50, 50]; //ZQUIERDA, DERECHA, ARRIBA, ABAJO
 const margins_3 = [50, 50, 50, 50]; //ZQUIERDA, DERECHA, ARRIBA, ABAJO
 
+const CHESS_SQUARES = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"];
 
+const JUGADAS_POSIBLES = ["Nf3", "c4", "d4", "e3", "e4"];
 
 SVG1.attr("width", WIDTH_VIS_1).attr("height", HEIGHT_VIS_1);
 SVG2.attr("width", WIDTH_VIS_2).attr("height", HEIGHT_VIS_2);
@@ -24,14 +33,11 @@ SVG3.attr("width", WIDTH_VIS_3).attr("height", HEIGHT_VIS_3);
 
 cargar_datos();
 
-
-
 function cargar_datos() {
     d3.csv(data_vis_1).then(function(d) {
         
         var data_1 = {};
-      
-        
+    
         d.forEach(function(d) {
           d.Nf3 = +d.Nf3;
           d.c4 = +d.c4;
@@ -75,6 +81,10 @@ function mostrar_grafico(data_1){
         };
         });
 
+    let firstKey = Object.keys(data_1)[0];
+    let seleccionados = Object.values(data_1[firstKey]).map(item => false)
+    console.log(seleccionados)
+
     var series = d3.stack()
         .keys(["Nf3", "c4", "d4", "e3", "e4"])
         .order(d3.stackOrderNone)
@@ -104,7 +114,10 @@ function mostrar_grafico(data_1){
         .attr("x", function(d) { return xScale(d.data.elo_intervalo); })
         .attr("y", function(d) { return yScale(d[1]); })
         .attr("height", function(d) { return yScale(d[0]) - yScale(d[1]); })
-        .attr("width", xScale.bandwidth());
+        .attr("width", xScale.bandwidth())
+        .on('click', (event, d) => {
+            preprocesarNodo_Enlace(d.data.elo_intervalo, data_1[d.data.elo_intervalo]);
+        });
     
     // Añade ejes
     SVG1.append("g")
@@ -115,7 +128,7 @@ function mostrar_grafico(data_1){
         .attr("transform", "translate(" + margins_1[0] + ", 0)")
         .call(d3.axisLeft(yScale));
 
-    preprocesarNodo_Enlace(["e4", "d4"], "700-799", data_1["[700, 800)"])
+    preprocesarNodo_Enlace(["e4", "e3", "d4", "Nf3", "c4"], "1000-1099", data_1["[1100, 1200)"])
 
 }
 
@@ -123,10 +136,9 @@ function mostrar_grafico(data_1){
 function nodo_enlace(data, jugadas, elo, data_1){
     console.log(data_1)
 
-    svgHeight = HEIGHT_VIS_2 - margins_2[2] - margins_2[3];
-    svgWidth = WIDTH_VIS_2 - margins_2[0] - margins_2[1];
-
     let dataArray = Object.values(data);
+
+    let svg = SVG2;
 
     // nos quedamos con los datos del rango de elo
     let datos = data[elo]
@@ -182,23 +194,31 @@ function nodo_enlace(data, jugadas, elo, data_1){
     console.log(MAX_OPENING, MIN_OPENING)
 
     // create a 'g' element for each data entry, to make a node-link diagram with d3
-
-    let numRegions = Object.keys(sortedData).length;
-    let regionHeight = HEIGHT_VIS_2 / numRegions; 
-    let regionWidth = WIDTH_VIS_2 / numRegions;
-
     let firstMovesKeys = Object.keys(sortedData);
 
+    let numFirstMoves = firstMovesKeys.length;
+
+    let svgWidth = WIDTH_VIS_2;
+    let svgHeight = HEIGHT_VIS_2;
+    
+
+    let cols = Math.min(2, numFirstMoves); // Maximum of 2 columns
+    let rows = Math.ceil(numFirstMoves / cols);
+
+    let regionWidth = svgWidth / cols;
+    let regionHeight = svgHeight / rows;
+
     firstMovesKeys.forEach((move, i) => {
-        SVG2.append("g")
-        .attr("class", "region region-" + move)
-        .attr("transform", `translate(${i * regionWidth}, 0)`);
+        let colIndex = i % cols;
+        let rowIndex = Math.floor(i / cols);
+
+        svg.append("g")
+       .attr("class", "region region-" + move)
+       .attr("transform", `translate(${colIndex * regionWidth}, ${rowIndex * regionHeight})`);
     });
 
-    let svg = SVG2;
-
     let datasets = firstMovesKeys.map(move => {
-        let nodes = [{id: move, type: 'firstMove', sizeVariable: data_1[move], fixedY: 150}];
+        let nodes = [{id: move, type: 'firstMove', sizeVariable: data_1[move], fixedY: 50}];
         let links = [];
     
         Object.keys(sortedData[move]).forEach(opening => {
@@ -215,7 +235,7 @@ function nodo_enlace(data, jugadas, elo, data_1){
         let simulation = d3.forceSimulation(dataset.nodes)
             .force("link", d3.forceLink(dataset.links).id(d => d.id).distance(50))
             .force("charge", d3.forceManyBody().strength(-5000))
-            .force("center", d3.forceCenter(regionWidth / 2, svgHeight / 2))
+            .force("center", d3.forceCenter(regionWidth / 2, svgHeight / 5))
             .force("y", d3.forceY(d => d.fixedY ? d.fixedY : svgHeight / 2))
             .force("x", d3.forceX(regionWidth / 2));
 
@@ -236,9 +256,7 @@ function nodo_enlace(data, jugadas, elo, data_1){
             .style("fill", d => d.type === 'firstMove' ? "blue" : "red");
 
         function calculateFontSize(radius) {
-                // proportional to the radius
-                console.log(radius)
-                return 13 + radius / 2;
+                return Math.min(10 + radius / 2, 15);
             }
         
         let labels = regionGroup.selectAll(".label")
@@ -271,32 +289,11 @@ function nodo_enlace(data, jugadas, elo, data_1){
     return;
 }
 
+function chess_graph(opening){
+    let svg = SVG3;
+    let svgWidth = WIDTH_VIS_3;
+    let svgHeight = HEIGHT_VIS_3;
 
-
-function crearSatelites(dataset, categoria, filtrar_dataset, ordenar_dataset) {
-    // 1. Actualizo nombre de un H4 para saber qué hacer con el dataset
-    let texto = `Categoria: ${categoria} - Filtrar: ${filtrar_dataset} - Orden: ${ordenar_dataset}`
-    d3.selectAll("#selected").text(texto)
-
-    // 2. Nos quedamos con los satelites asociados a la categoría seleccionada
-    console.log(categoria)
-    let datos = dataset.filter(d => CATEGORIAS_POR_PLANETA[d.planet] == categoria)
-
-    // 3. Filtrar, cuando corresponde, por magnitud
-    // Completar aquí
-    console.log(filtrar_dataset)
-
-
-    // 4. Quedarnos con solo 30 satelites. No editar esta línea
-    datos = datos.slice(0, 30);
-    console.log(datos)
-
-    // 5. Ordenar, según corresponda, los 30 satelites. Completar aquí
-    console.log(ordenar_dataset)
-
-
-
-    // 6. Confeccionar la visualización
-
-
+    let numSquares = 8;
+    let squareSize = Math.min(svgWidth, svgHeight) / numSquares;
 }
